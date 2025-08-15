ff7cb0ab32ae24d5132708c22ab0d104e5de0f45
Marcos <mvmendesferreira@id.uff.br>
2025-08-15 10:47:01 -0300

    add: colocando assests e começando Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
+import Header from "./Components/Header/Header"
 
 
 function App() {
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import logo from "../../assets/Logo.png"
+import carrinho from "../../assets/Carrinho.png"
+import favorito from "../../assets/Favorito.png"
+import user from "../../assets/User.png"
+import styles from "./styles.module.css"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={logo} alt="logo" />
+
+            <nav>
+                <p>PIJAMAS</p>
+                <p>FEMININO</p>
+                <p>MASCULINO</p>
+                <p>INFANTIL</p>
+            </nav>
+
+            <div>
+                <div>
+                    <img src={carrinho} alt="carrinho" />
+                    <img src={favorito} alt="favorito" />
+                </div>
+                <img src={user} alt="login" />
+            </div>
+        </header>
+    )
+}
