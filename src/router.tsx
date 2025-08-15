535025093516e1127e52b45db764e02bbf18899f
Marcos <mvmendesferreira@id.uff.br>
2025-08-15 10:35:04 -0300

    add: criando repositório PijamINha

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./RootLayout";
+
+const router = createBrowserRouter([
+    // {
+    //     path: "/",
+    //     element: <Login />,
+    // },
+    {
+        path: "/xxx",
+        element: <RootLayout />,
+        children: [
+        ]
+    }
+])
+
+
+export default router;
