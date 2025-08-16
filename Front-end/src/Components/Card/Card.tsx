16a79024bbaa07ebf956c9b2b98e82505581d363
Marcos <mvmendesferreira@id.uff.br>
2025-08-16 15:38:37 -0300

    add: colocando hover e opção de clicar no coração do card

--- Front-end/src/Components/Card/Card.tsx
+++ Front-end/src/Components/Card/Card.tsx
@@ -1,18 +1,26 @@
 import styles from "./styles.module.css";
 import coracao from "../../Assets/Coracao.png";
 import desconto from "../../Assets/Desconto.png";
-import favoritado from "../../Assets/Favoritado.png";
+import favoritadoimg from "../../Assets/Favoritado.png";
 import type { CardPijama } from "../../Types/CardPijama";
+import { useState } from "react";
+
 
 export default function Card(props: CardPijama) {
+    const [favorited, setFavorited] = useState(props.favorite);
+
     return (
         <div className={styles.card}>
             <div className={styles.images}>
-                {props.favorite ? (
-                    <img src={favoritado} alt="coracao" className={styles.coracao}/>
+
+                {favorited ? (
+                    <img src={favoritadoimg} alt="coracao" className={styles.coracao} onClick={() => setFavorited(!favorited)}/>
                 ) : (
-                    <img src={coracao} alt="coracao" className={styles.coracao}/>
+                    <img src={coracao} alt="coracao" className={styles.coracao} onClick={() => setFavorited(!favorited)}/>
                 )}
+
+                {/* TEM QUE ADICIONAR TRATAMENTO DE CLICAR NO CORAÇÃO E FAVORITAR NA BASE */}
+
                 {props.on_sale && (<img src={desconto} className={styles.desconto}/>)}
                 <img src={props.image} alt="img fundo" className={styles.fundo}/> 
             </div>
--- Front-end/src/router.tsx
+++ Front-end/src/router.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
                             name="Pijama feminino longo - estampa poá daksdajs ndna jsndjs najn dsjan jdn ajn"
                             price={79.99}
                             image="https://images.tcdn.com.br/img/img_prod/460977/pijama_macacao_kigurumi_adulto_unissex_stitch_lilo_eamp_stitch_disney_mkp_119771_1_ccb98b402f9860e36ae7c93ee82387c7.jpg"
-                            favorite={false}
+                            favorite={true}
                             on_sale={true}
                             sale_percent={10}
                         />,
