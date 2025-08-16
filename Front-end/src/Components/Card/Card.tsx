87a7de3c41b350ed0a9b31a531d3e040863ddcfc
Marcos <mvmendesferreira@id.uff.br>
2025-08-16 15:15:24 -0300

    add: colocando os props no card pijama

--- Front-end/src/Components/Card/Card.tsx
+++ Front-end/src/Components/Card/Card.tsx
@@ -2,22 +2,34 @@ import styles from "./styles.module.css";
 import coracao from "../../Assets/Coracao.png";
 import desconto from "../../Assets/Desconto.png";
 import favoritado from "../../Assets/Favoritado.png";
-import FUNDOCARDTESTE from "../../Assets/FundoCardTeste.png"; //TIRAR E COLOCAR A IMAGEM DO PROPS
+import type { CardPijama } from "../../Types/CardPijama";
 
-export default function Card() {
+export default function Card(props: CardPijama) {
     return (
         <div className={styles.card}>
             <div className={styles.images}>
-                <img src={coracao} alt="coracao" className={styles.coracao}/>
-                <img src={desconto} alt="desconto" className={styles.desconto}/>
-                <img src={FUNDOCARDTESTE} alt="img fundo" className={styles.fundo}/>
+                <img src={props.favorite ? favoritado : coracao} alt="coracao" className={styles.coracao}/>
+                <img src={props.on_sale ? desconto: ""} className={styles.desconto}/>
+                <img src={props.image} alt="img fundo" className={styles.fundo}/> 
             </div>
 
             <div className={styles.info}>
-                <h1>Nome do pijama</h1>
-                <p className={styles.precoOriginal}>R$ 78,00</p>
-                <h2>R$ 40,00</h2>
-                <p className={styles.parcelas}>6x de <strong>13,15</strong></p>
+                <h1>{props.name}</h1>
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "12px"}}>
+                    <div className={styles.preco}>
+                        {props.on_sale && (
+                        <p className={styles.precoOriginal}>
+                            R$ {props.price.toFixed(2).replace(".", ",")}
+                        </p>
+                        )}
+                        {props.on_sale ? (
+                            <h2>R$ {(props.price - (props.price * props.sale_percent / 100)).toFixed(2).replace(".", ",")}</h2>
+                        ) : (
+                            <h2>R$ {props.price.toFixed(2).replace(".", ",")}</h2>
+                        )}
+                    </div>
+                    <p className={styles.parcelas}>6x de <strong>{(props.price / 6).toFixed(2).replace(".", ",")}</strong></p>
+                </div>
             </div>
         </div>
     )
--- Front-end/src/router.tsx
+++ Front-end/src/router.tsx
@@ -13,7 +13,14 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <Card />,
+                element: <Card 
+                            name="Pijama feminino longo - estampa poá"
+                            price={79.99}
+                            image="https://images.tcdn.com.br/img/img_prod/460977/pijama_macacao_kigurumi_adulto_unissex_stitch_lilo_eamp_stitch_disney_mkp_119771_1_ccb98b402f9860e36ae7c93ee82387c7.jpg"
+                            favorite={false}
+                            on_sale={true}
+                            sale_percent={10}
+                        />,
             }
         ]
     }
