dbe40ee39049233babde83bb93c48f32dc74c877
Lucasneca <lucasneca6@gmail.com>
2025-08-16 21:53:31 -0300

    feat: implementação de registro e autenticação de usuarios

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,19 +1,30 @@
 import fastify from 'fastify'
-import z, { ZodError } from 'zod'
+import { ZodError } from 'zod'
 import cors from '@fastify/cors'
+import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
+import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
 app.register(cors, {
     origin: true,
-    allowedHeaders: ["POST", "GET", "PATCH", "DELETE", "PUT"]
+    methods: ["POST", "GET", "PATCH", "DELETE", "PUT"]
 })
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+})
+
+app.register(usersRoutes)
 
 
 app.setErrorHandler((error, request, reply) => {
     if (error instanceof ZodError) {
-        return reply.status(400).send({ message: 'erro de validação', issues: z.treeifyError(error) })
+        return reply.status(400).send({ message: 'Erro de validação.', issues: error.format() })
     }
-    return reply.status(500).send({message: 'erro interno do servidor', error: error.message})
-
+    if (env.NODE_ENV !== 'production') {
+        console.error(error)
+    }
+    return reply.status(500).send({ message: 'Erro interno do servidor.' })
 })
