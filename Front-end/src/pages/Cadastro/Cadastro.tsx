9673558573a274480eb720418f6e7fcf8f316108
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-18 17:48:59 -0300

    feat: inicializacao da funcao do Cadastro e importacao do styles

---
+++ Front-end/src/pages/Cadastro/Cadastro.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css"
+
+export default function Cadastro(){
+
+    return(
+        <div className={styles.mainCadastro}>
+            
+        </div>
+    )
+}
