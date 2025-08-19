d4e285556a8e887963df377b5f3208fe87a1a36a
Marcos <mvmendesferreira@id.uff.br>
2025-08-19 13:22:01 -0300

    feat: criando estrutura da pagina individual e componente de info

---
+++ Front-end/src/Components/InfoPijamaIndividual/InfoPijamaIndividual.tsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.css';
+
+export default function InfoPijamaIndividual() {
+
+    return (
+        <div>
+            <div className={styles.tituloContainer}>
+
+            </div>
+
+            <div className={styles.priceContainer}>
+
+            </div>
+
+            <div className={styles.tamanhoContainer}>
+
+            </div>
+
+            <div className={styles.quantidadeContainer}>
+
+            </div>
+
+            <div className={styles.botoesContainer}>
+                
+            </div>
+        </div>
+    )
+}
---
+++ Front-end/src/pages/Individual/Individual.tsx
@@ -0,0 +1,27 @@
+
+
+export default function Individual() {
+
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+
+                
+                
+            </div>
+
+
+
+
+
+            <div>
+                <img src="" alt="" /> 
+
+                <img src="" alt="" />
+
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
