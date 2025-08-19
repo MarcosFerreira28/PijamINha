3ecef88728e56df2d52cec7610fa93d4d8a61048
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-19 00:32:06 -0300

    feat(Cadastro): criacao da validacao completa de formulario com Zod da página de cadastro

--- Front-end/src/pages/Cadastro/Cadastro.tsx
+++ Front-end/src/pages/Cadastro/Cadastro.tsx
@@ -1,41 +1,64 @@
 import { useForm } from "react-hook-form"
 import styles from "./styles.module.css"
+import z from "zod"
+import cpf from 'cpf'
 
-export default function Cadastro(){
-    const { register } = useForm()
+const useSchema = z.object({
+    nome: z.string().regex(/^[^/d]+$/ , {
+        message: 'Não pode ter números'
+    }),
+    usuario: z.string().refine(value => value.trim().length > 0, { message: 'Não pode ter espaços' }),
+    email: z.string().nonempty('O E-mail não pode ser vazio').refine(value => z.string().email().safeParse(value).success, { 
+        message: 'O e-mail não é válido'
+    }),
+    senha: z.string().nonempty('Senha não pode ser vazia').min(6, 'Deve ter no mínimo 6 caracteres').refine(value => value.trim().length > 0, { message: 'Não pode ter espaços' }),
+    confirmarSenha: z.string().nonempty('A confirmação de senha não pode ser vazia').refine(data => data.senha === data.confirmarSenha, {
+        message: 'As senhas não coincidem',
+        path: ['confimarSenha']
+    })
+})
 
-    return(
+export default function Cadastro() {
+    const { register, handleSubmit, reset } = useForm()
+
+    function createUser(data) {
+        console.log(data)
+        reset()
+    }
+
+    return (
         <div className={styles.mainCadastro}>
 
-           <div className={styles.cardCadastro}>
+            <div className={styles.cardCadastro}>
                 <h1>Registre-se</h1>
-                
-                <div className={styles.inputs}>
-                    <input type="text" 
-                    placeholder="Nome"
-                    {...register('nome')}
-                    />
-                    <input type="text" 
-                    placeholder="Nome de Usuário"
-                    {...register('usuario')}
-                    />
-                    <input type="email" 
-                    placeholder="E-mail"
-                    {...register('email')}
-                    />
-                    <input type="password" 
-                    placeholder="Senha"
-                    {...register('senha')}
-                    />
-                    <input type="password" 
-                    placeholder="Confirmar senha"
-                    {...register('confirmarSenha')}
-                    />
-                </div>
-                
+
+                <form onSubmit={handleSubmit(createUser)}>
+                    <div className={styles.inputs}>
+                        <input type="text"
+                            placeholder="Nome"
+                            {...register('nome')}
+                        />
+                        <input type="text"
+                            placeholder="Nome de Usuário"
+                            {...register('usuario')}
+                        />
+                        <input type="email"
+                            placeholder="E-mail"
+                            {...register('email')}
+                        />
+                        <input type="password"
+                            placeholder="Senha"
+                            {...register('senha')}
+                        />
+                        <input type="password"
+                            placeholder="Confirmar senha"
+                            {...register('confirmarSenha')}
+                        />
+                    </div>
+                </form>
                 <button className={styles.btnRegistrar}>REGISTRAR</button>
-            
-            </div> 
+
+            </div>
         </div>
     )
 }
