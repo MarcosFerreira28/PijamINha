587819030eb0fc4062eb8930a222162ca20fae5e
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-19 14:33:57 -0300

    feat: estruturacao html da pagina login

--- Front-end/src/pages/Login/Login.tsx
+++ Front-end/src/pages/Login/Login.tsx
@@ -1,8 +1,35 @@
 import styles from "./styles.module.css"
+import { Link } from "react-router-dom" 
 
-export default function Login(){
+export default function Login() {
 
-    return(
-        <></>
+    return (
+        <div className={styles.mainLogin}>
+
+            <div className={styles.loginCard}>
+
+                <div className={styles.tituloEdesc}>
+                    <h1>Login</h1>
+                    <h2>Faça login para ter acesso aos pijamas dos seus <span>sonhos!</span></h2>
+                </div>
+
+                <form className={styles.inputsEbotoes}>
+                    <div className={styles.cadaInput}>
+                        <input type="text"
+                            placeholder="Usuário ou E-mail"
+                        />
+                    </div>
+                    <div className={styles.cadaInput}>
+                        <input type="text"
+                            placeholder="Senha"
+                        />
+                    </div>
+                    <button className={styles.btnEsqueciSenha}>Esqueci a minha senha</button>
+                    <div className={styles.barraBranca}></div>
+                    <Link to='/cadastro'>Cadastre-se</Link>
+                </form>
+
+            </div>
+        </div>
     )
 }
