0db4c2b1c9df07e85c89ab051d915a229e6ad626
Lucasneca <lucasneca6@gmail.com>
2025-08-18 23:16:06 -0300

    teste

--- Back-end/src/http/controllers/users/routes.ts
+++ Back-end/src/http/controllers/users/routes.ts
@@ -1,10 +1,20 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './register';
 import { authenticate } from './authenticate';
+import { verifyJwt } from '@/http/middlewares/authenticate';
+import { profile } from './profile';
+import { update } from './update';
+import { deleteUser } from './delete';
 
 export async function usersRoutes(app: FastifyInstance) {
-    
+
     app.post('/users', register);
 
     app.post('/sessions', authenticate);
+
+    app.get('/me', { onRequest: [verifyJwt] }, profile);
+
+    app.patch('/users', { onRequest: [verifyJwt] }, update);
+
+    app.delete('/users', { onRequest: [verifyJwt] }, deleteUser);
 }
