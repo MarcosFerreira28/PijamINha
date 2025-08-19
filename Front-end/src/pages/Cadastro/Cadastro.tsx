42c3e10c1abe501e248745f88e0a9751423b910a
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-18 23:53:55 -0300

    feat: instalacao do zod e do hook form e do registro de cada input com seus respectivos nomes

--- Front-end/src/pages/Cadastro/Cadastro.tsx
+++ Front-end/src/pages/Cadastro/Cadastro.tsx
@@ -1,6 +1,8 @@
+import { useForm } from "react-hook-form"
 import styles from "./styles.module.css"
 
 export default function Cadastro(){
+    const { register } = useForm()
 
     return(
         <div className={styles.mainCadastro}>
@@ -11,18 +13,23 @@ export default function Cadastro(){
                 <div className={styles.inputs}>
                     <input type="text" 
                     placeholder="Nome"
+                    {...register('nome')}
                     />
                     <input type="text" 
                     placeholder="Nome de Usuário"
+                    {...register('usuario')}
                     />
                     <input type="email" 
                     placeholder="E-mail"
+                    {...register('email')}
                     />
                     <input type="password" 
                     placeholder="Senha"
+                    {...register('senha')}
                     />
                     <input type="password" 
                     placeholder="Confirmar senha"
+                    {...register('confirmarSenha')}
                     />
                 </div>
                 
