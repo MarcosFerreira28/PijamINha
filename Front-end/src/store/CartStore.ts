f6be8984f1569f6f772f2a18605cf31e9358c63a
Marcos <mvmendesferreira@id.uff.br>
2025-08-20 18:15:37 -0300

    feat: criando as stores de cart

---
+++ Front-end/src/store/CartStore.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { create } from "zustand";
+import type CartStore from "../interfaces/CartStore";
+
+const pijamas = await axios.get("http://localhost:3001/pijamas");
+
+const useCartStore = create<CartStore>((set) => (
+    {
+        availablePijamas: pijamas.data,
+        cart: [],
+        addToCart: (pijama) => set((state) => ({ cart: [...state.cart, pijama]})),
+        removeFromCart: (id) => set((state) => {
+            const index = state.cart.findIndex((pijama) => pijama.id === id);
+            if (index === -1) return { cart: state.cart };
+            return {
+                cart: [
+                    ...state.cart.slice(0, index),
+                    ...state.cart.slice(index + 1)
+                ]
+            };
+        })
+    }
+))
+
+export default useCartStore;
