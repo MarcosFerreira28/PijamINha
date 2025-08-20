3fb94939645d1ce5364c0de0af25771cdd94b605
Marcos <mvmendesferreira@id.uff.br>
2025-08-20 11:58:48 -0300

    add: adicionando cards com desconto requisitando o json na home

--- Front-end/src/Types/Pijama.ts
+++ Front-end/src/Types/Pijama.ts
@@ -1,5 +1,6 @@
 export type Pijama = {
     //talvez inserir o id
+    id: number;
     name: string;
     description: string;
     price: number;
@@ -10,9 +11,10 @@ export type Pijama = {
     favorite: boolean;
     on_sale: boolean;
     sale_percent?: number;
-    menor: boolean;
     //Verificar se as outras colunas da tabela deveriam estar nesse tipo
     // eu acredito que não
 }
 
-export type CardPijama = Pick<Pijama, 'name' | 'price' | 'image' | 'favorite' | 'on_sale' | 'sale_percent' | 'menor'>;
+export type CardPijama = Pick<Pijama, 'name' | 'price' | 'image' | 'favorite' | 'on_sale' | 'sale_percent'> & {
+    menor: boolean
+};
