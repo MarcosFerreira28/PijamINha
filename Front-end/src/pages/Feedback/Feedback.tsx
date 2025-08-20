8dbdd42252975e3b101c3686b6f5ce3c7f623bef
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-20 20:10:38 -0300

    feat(Feedback): estruturação do html da página

--- Front-end/src/pages/Feedback/Feedback.tsx
+++ Front-end/src/pages/Feedback/Feedback.tsx
@@ -1,6 +1,38 @@
+import styles from "./styles.module.css"
 
-export default function Feedback(){
-    return(
-        <></>
+export default function Feedback() {
+    return (
+        <div className={styles.mainFeedback}>
+
+            <div className={styles.feedbackCard}>
+                <div className={styles.tituloEtexto}>
+                    <h1 className={styles.titulo}>Feedback</h1>
+                    <div className={styles.texto}>
+                        <p>Fale um pouco sobre a sua</p>
+                        <p>experiência com a nossa loja!</p>
+                    </div>
+                </div>
+                <form>
+                    <div className={styles.inputs}>
+                        <input
+                            type="text"
+                            placeholder="Nome Completo"
+                            className={styles.nome}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Descrição Detalhada"
+                            className={styles.descricao}
+                        />
+                    </div>
+                    <div className={styles.avaliacao}>
+
+                    </div>
+                    <div className={styles.enviarContainer}>
+                        <button className={styles.btnEnviar}></button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
