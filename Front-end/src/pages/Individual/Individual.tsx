f71c9e0820bade7303ec5963b6e5865b6eff34b8
Marcos <mvmendesferreira@id.uff.br>
2025-08-19 21:53:18 -0300

    add: colocando as imagens de estação etc

--- Front-end/src/pages/Individual/Individual.tsx
+++ Front-end/src/pages/Individual/Individual.tsx
@@ -1,4 +1,5 @@
 import InfoPijamaIndividual from "../../Components/InfoPijamaIndividual/InfoPijamaIndividual";
+import styles from "./styles.module.css";
 
 import masculino from '../../Assets/Masculino.svg';
 import feminino from '../../Assets/Feminino.svg';
@@ -11,11 +12,14 @@ import infantil from '../../Assets/Infantil.svg';
 import ambos from '../../Assets/Ambos.svg';
 
 export default function Individual() {
+    const season = "inverno";
+    const gender = "Feminino";
+    const type = "Adulto";
 
     return (
-        <div>
-            <div>
-                <img src="" alt="" />
+        <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <img className={styles.imagemPijama} src="https://images.tcdn.com.br/img/img_prod/460977/pijama_macacao_kigurumi_adulto_unissex_stitch_lilo_eamp_stitch_disney_mkp_119771_1_ccb98b402f9860e36ae7c93ee82387c7.jpg" alt="" />
 
                 <InfoPijamaIndividual />
 
@@ -23,13 +27,27 @@ export default function Individual() {
 
 
 
-            <div>
-                <img src="" alt="" /> 
+            <div className={styles.imagensContainer}>
+                {season.toLowerCase() === "inverno" ? <img src={inverno} alt="" /> : (
+                    season.toLowerCase() === "verão" ? <img src={verao} alt="" /> : null
+                )}
 
-                <img src="" alt="" />
+                {gender.toLowerCase() === "feminino" ? <img src={feminino} alt="" /> : (
+                    gender.toLowerCase() === "masculino" ? <img src={masculino} alt="" /> : (
+                        gender.toLowerCase() === "unissex" ? <img src={unissex} alt="" /> : (
+                            gender.toLowerCase() === "família" ? <img src={familia} alt="" /> : null
+                        )
+                    )
+                )}
 
-                <img src="" alt="" />
+                {type.toLowerCase() === "adulto" ? <img src={adulto} alt="" /> : (
+                    type.toLowerCase() === "infantil" ? <img src={infantil} alt="" /> : (
+                        type.toLowerCase() === "ambos" ? <img src={ambos} alt="" /> : null
+                    )
+                )}
             </div>
+
+            <SobrePijama />
         </div>
     )
 }
