a6f394ded63942bc0842096b8ee3f8364d27beb7
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-21 01:00:01 -0300

    feat(Estrelas.tsx): criacao do componente estrela e sua estilizacao com seu funcionamento

--- Front-end/src/pages/Feedback/Feedback.tsx
+++ Front-end/src/pages/Feedback/Feedback.tsx
@@ -1,10 +1,43 @@
 import Estrelas from "../../Components/Estrelas/Estrelas"
 import styles from "./styles.module.css"
+import { useState } from "react"
 
+type FeedbackType = {
+    name: string;
+    description: string;
+    rating: number;
+}
 export default function Feedback() {
+    const [formData, setFormData] = useState<FeedbackType>({
+        name: '',
+        description: '',
+        rating: 0,
+    });
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleRatingChange = (newRating: number) => {
+        setFormData({
+            ...formData,
+            rating: newRating,
+        });
+        console.log("Avaliação:", newRating);
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log("Dados:", formData);
+
+    };
+
     return (
         <div className={styles.mainFeedback}>
-
             <div className={styles.feedbackCard}>
                 <div className={styles.tituloEtexto}>
                     <h1 className={styles.titulo}>Feedback</h1>
@@ -13,31 +46,35 @@ export default function Feedback() {
                         <p>experiência com a nossa loja!</p>
                     </div>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className={styles.inputs}>
                         <input
+                            name="name"
                             type="text"
                             placeholder="Nome Completo"
                             className={styles.nome}
+                            value={formData.name}
+                            onChange={handleInputChange}
                         />
-                        <textarea 
-                        name="" 
-                        id="" 
-                        placeholder="Descição Detalhada"
-                        className={styles.descricao}
+                        <textarea
+                            name="description"
+                            id=""
+                            placeholder="Descição Detalhada"
+                            className={styles.descricao}
+                            value={formData.description}
+                            onChange={handleInputChange}
                         ></textarea>
-                    </div> 
+                    </div>
 
-                        <Estrelas />
+                    <Estrelas onRate={handleRatingChange} />
 
                     <div>
                     </div>
                     <div className={styles.enviarContainer}>
-                        <button className={styles.btnEnviar}>ENVIAR</button>
+                        <button className={styles.btnEnviar} type="submit">ENVIAR</button>
                     </div>
                 </form>
             </div>
         </div>
-    )
+    );
 }
-
